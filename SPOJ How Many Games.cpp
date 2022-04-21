5452bb76497f4afe054b036cf9a381440499e804
KHALED AHMMED ANIK <59437962+Khaled227@users.noreply.github.com>
2022-04-22 00:06:14 +0600

    first commit

---
+++ SPOJ How Many Games.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    string s, avg;
+
+    long long noDecDigit=0, isDec=0 , denominator,gcd, numinator;
+
+    cin>>s;
+
+    for(int i=0;i< s.size();i++){
+        if(isDec)noDecDigit++;//counting number of digits after decimal
+        if (s[i]=='.')isDec=1;// check '.' got or not
+        if (s[i]!='.')avg+=s[i];//taking average(s) string in avg without "."
+    }
+
+    denominator = ceil(pow(10,noDecDigit)); //calculate 10^(no of digits after decimal)
+
+    numinator=stoi(avg); // convert avg string to long long
+
+    gcd=__gcd(numinator,denominator);// find the gcd
+
+    cout<<denominator/gcd<<"\n"; 
+
+}
+
+int main() {
+    int t = 1;
+    cin>>t;
+
+    //test case
+    while (t--)
+        solve();
+}
